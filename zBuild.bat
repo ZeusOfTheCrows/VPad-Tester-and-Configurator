c534fef31f6d60c9f2b82970edcd0b94a71ebd0e
ZeusOfTheCrows <zeusofthecrows+github@gmail.com>
2022-01-25 22:34:45 +0000

    move eboot files to subfolders (workaround for -tzip)

--- zBuild.bat
+++ zBuild.bat
@@ -3,5 +3,5 @@
 ::vita-mksfoex -s TITLE_ID=%id%88888 "%title%" ..\src\sce_sys\param.sfo
 .\lpp\vita-mksfoex -s TITLE_ID=%id% "%title%" .\src\sce_sys\param.sfo
 ::                : =- replaces " " with "-"
-.\lpp\7z a -tzip "%title: =-%.safe.vpk"   -r .\src\* .\lpp\eboot_safe.bin
-.\lpp\7z a -tzip "%title: =-%.unsafe.vpk" -r .\src\* .\lpp\eboot_unsafe.bin
+.\lpp\7z a -tzip "%title: =-%.safe.vpk"   -r .\src\* .\lpp\eboot_safe.bin\eboot.bin
+.\lpp\7z a -tzip "%title: =-%.unsafe.vpk" -r .\src\* .\lpp\eboot_unsafe.bin\eboot.bin
